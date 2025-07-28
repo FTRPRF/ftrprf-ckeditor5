422e6500704a56103319da6e7a7ebc1b9597ed01
Badlapje <koen@cornelis.it>
2025-07-28 10:53:23 +0200

    Create own plugin

--- packages/ckeditor5-build-decoupled-document/src/ckeditor.ts
+++ packages/ckeditor5-build-decoupled-document/src/ckeditor.ts
@@ -54,7 +54,6 @@ import { TextTransformation } from '@ckeditor/ckeditor5-typing';
 import { CloudServices } from '@ckeditor/ckeditor5-cloud-services';
 import { FindAndReplace } from '@ckeditor/ckeditor5-find-and-replace';
 import { RemoveFormat } from '@ckeditor/ckeditor5-remove-format';
-import { CodeBlock } from '@ckeditor/ckeditor5-code-block';
 
 // eslint-disable-next-line ckeditor5-rules/allow-imports-only-from-main-package-entry-point
 import ClickObserver from '@ckeditor/ckeditor5-engine/src/view/observer/clickobserver.js';
@@ -71,6 +70,7 @@ import { Modal } from './plugins/modal/index.js';
 // @ts-ignore
 import { StyledLink } from './plugins/styledLink/index.js';
 // @ts-ignore
+import {CodeBlock} from "./plugins/codeblock";
 import { FullScreen } from './plugins/fullScreen/index.js';
 import { Source } from './plugins/source/index.js';
 import { OwnImagePlugin } from './plugins/image/index.js';
